d91aef35d73cb96460cffbcdb69efef5176b55b2
drake90001 <cheddarbeand@gmail.com>
2017-01-25 20:39:50 -0600

    Update offsets.c

--- yalu102/offsets.c
+++ yalu102/offsets.c
@@ -20,9 +20,9 @@ void init_offsets() {
     printf("version: %s\n", u.version);
     printf("machine: %s\n", u.machine);
     
-    allproc_offset = 0x5a8438;
+    allproc_offset = 0x5a4148;
     procoff = 0x360;
-    rootvnode_offset = 0x5ae0b8;
+    rootvnode_offset = 0x5aa0b8;
 
     if (strcmp(u.version, "Darwin Kernel Version 16.1.0: Thu Sep 29 21:56:12 PDT 2016; root:xnu-3789.22.3~1/RELEASE_ARM64_S8000") == 0) {
         allproc_offset = 0x5a4148;
@@ -32,6 +32,10 @@ void init_offsets() {
         allproc_offset = 0x5a4148;
         procoff = 0x360;
         rootvnode_offset = 0x5aa0b8; /* this one is wrong i think but no devices to test */
+    } else if (strcmp(u.version, "Darwin Kernel Version 16.3.0: Tue Nov 29 21:40:09 PST 2016; root:xnu-3789.32.1~4/RELEASE_ARM64_S8000") == 0) {
+        allproc_offset = 0x5a8438;
+    procoff = 0x360;
+    rootvnode_offset = 0x5ae0b8;
     } else {
         printf("missing offset, prob crashing\n");
     }
