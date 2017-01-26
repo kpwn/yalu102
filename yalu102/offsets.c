5db2fa3a7459ecb7074ee56ddcfb3cd85295b42e
drake90001 <cheddarbeand@gmail.com>
2017-01-25 23:27:37 -0600

    add iPhone 5s

--- yalu102/offsets.c
+++ yalu102/offsets.c
@@ -40,6 +40,10 @@ void init_offsets() {
         allproc_offset = 0x5b8468;
         procoff = 0x360; // iphone 6, credit to @Andywiik
         rootvnode_offset = 0x5be0b8;
+    } else if (strcmp(u.version, "Darwin Kernel Version 16.3.0: Tue Nov 29 21:40:09 PST 2016; root:xnu-3789.32.1~4/RELEASE_ARM64_S5L8960X") == 0) {
+        allproc_offset = 0x5b20e0;
+        procoff = 0x360; // iphone 5s, @Slonick (github)
+        rootvnode_offset = 0x5b20b8;
     } else {
         printf("missing offset, prob crashing\n");
     }
