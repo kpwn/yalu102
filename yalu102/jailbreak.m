f78c029df2791876defae4faeb4e9b595f71f7f5
qwertyoruiop <qwertyoruiop@Mac-Pro.local>
2017-01-26 03:03:06 +0100

    bootstrap but substrate is broken

--- yalu102/jailbreak.m
+++ yalu102/jailbreak.m
@@ -511,15 +511,6 @@ void exploit(void* btn, mach_port_t pt, uint64_t kernbase, uint64_t allprocs)
 
     uint64_t fref = find_reference((uint32_t*)get_data_for_mode(0, SearchTextExec), text_exec_size, text_exec_base, idlesleep_handler+0xC) + text_exec_base;
     NSLog(@"fref at %llx", fref);
-    
-    
-    uint64_t tramp1 = physalloc(PSZ);
-    uint64_t tramp2 = physalloc(PSZ);
-    uint64_t tramp_idle = findphys_real(tramp1);
-    uint64_t tramp_deep = findphys_real(tramp2);
-
-    
-    NSLog(@"%llx", fref);
 
     /*
      first str
@@ -647,8 +638,90 @@ void exploit(void* btn, mach_port_t pt, uint64_t kernbase, uint64_t allprocs)
         WriteAnywhere32(v_mount + 0x71, v_flag);
     }
 
-    WriteAnywhere64(bsd_task+0x100, orig_cred);
-
+    {
+        char path[256];
+        uint32_t size = sizeof(path);
+        _NSGetExecutablePath(path, &size);
+        char* pt = realpath(path, 0);
+        
+        {
+            __block pid_t pd = 0;
+            NSString* execpath = [[NSString stringWithUTF8String:pt]  stringByDeletingLastPathComponent];
+            
+            int f = open("/.installed_yaluX", O_RDONLY);
+            
+            if (f == -1) {
+                NSString* tar = [execpath stringByAppendingPathComponent:@"tar"];
+                NSString* bootstrap = [execpath stringByAppendingPathComponent:@"bootstrap.tar"];
+                const char* jl = [tar UTF8String];
+                
+                unlink("/bin/tar");
+                unlink("/bin/launchctl");
+                
+                copyfile(jl, "/bin/tar", 0, COPYFILE_ALL);
+                chmod("/bin/tar", 0777);
+                jl="/bin/tar"; //
+                
+                chdir("/");
+                
+                posix_spawn(&pd, jl, 0, 0, (char**)&(const char*[]){jl, "--preserve-permissions", "-xvf", [bootstrap UTF8String], NULL}, NULL);
+                NSLog(@"pid = %x", pd);
+                waitpid(pd, 0, 0);
+                
+                
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"launchctl"];
+                jl = [jlaunchctl UTF8String];
+                
+                copyfile(jl, "/bin/launchctl", 0, COPYFILE_ALL);
+                chmod("/bin/launchctl", 0755);
+                
+                open("/.installed_yaluX", O_RDWR|O_CREAT);
+                open("/.cydia_no_stash",O_RDWR|O_CREAT);
+                
+                
+                system("echo '127.0.0.1 iphonesubmissions.apple.com' >> /etc/hosts");
+                system("echo '127.0.0.1 radarsubmissions.apple.com' >> /etc/hosts");
+                
+                system("/usr/bin/uicache");
+                
+                system("killall -SIGSTOP cfprefsd");
+                NSMutableDictionary* md = [[NSMutableDictionary alloc] initWithContentsOfFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist"];
+                
+                [md setObject:[NSNumber numberWithBool:YES] forKey:@"SBShowNonDefaultSystemApps"];
+                
+                [md writeToFile:@"/var/mobile/Library/Preferences/com.apple.springboard.plist" atomically:YES];
+                system("killall -9 cfprefsd");
+                
+            }
+            {
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"reload"];
+                char* jl = [jlaunchctl UTF8String];
+                unlink("/usr/libexec/reload");
+                copyfile(jl, "/usr/libexec/reload", 0, COPYFILE_ALL);
+                chmod("/usr/libexec/reload", 0755);
+                chown("/usr/libexec/reload", 0, 0);
+                
+            }
+            {
+                NSString* jlaunchctl = [execpath stringByAppendingPathComponent:@"0.reload.plist"];
+                char* jl = [jlaunchctl UTF8String];
+                unlink("/Library/LaunchDaemons/0.reload.plist");
+                copyfile(jl, "/Library/LaunchDaemons/0.reload.plist", 0, COPYFILE_ALL);
+                chmod("/Library/LaunchDaemons/0.reload.plist", 0644);
+                chown("/Library/LaunchDaemons/0.reload.plist", 0, 0);
+            }
+            
+        }
+    }
+    chmod("/private", 0777);
+    chmod("/private/var", 0777);
+    chmod("/private/var/mobile", 0777);
+    chmod("/private/var/mobile/Library", 0777);
+    chmod("/private/var/mobile/Library/Preferences", 0777);
     
+    system("echo 'really jailbroken'; /bin/launchctl load /Library/LaunchDaemons/0.reload.plist");
+    WriteAnywhere64(bsd_task+0x100, orig_cred);
+    
+
     NSLog(@"done");
 }
