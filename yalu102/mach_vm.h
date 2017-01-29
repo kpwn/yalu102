6e1f1c71e9ab5c7224b3fdd1723d00508dd781f0
Oliver Kuckertz <oliver.kuckertz@mologie.de>
2017-01-29 17:42:14 +0100

    Fix vm_address_t size regression

--- yalu102/mach_vm.h
+++ yalu102/mach_vm.h
@@ -3,6 +3,9 @@
 
 #include <mach/arm/vm_types.h>
 
+// evil hackfix: makes vm_address_t 64-bit
+#define vm_address_t mach_vm_address_t
+
 kern_return_t mach_vm_read_overwrite(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, mach_vm_address_t data, mach_vm_size_t *outsize);
 kern_return_t mach_vm_write(vm_map_t target_task, mach_vm_address_t address, vm_offset_t data, mach_msg_type_number_t dataCnt);
 kern_return_t mach_vm_protect(vm_map_t target_task, mach_vm_address_t address, mach_vm_size_t size, boolean_t set_maximum, vm_prot_t new_protection);
